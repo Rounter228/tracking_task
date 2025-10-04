4963b0898420860a10fbe403139ecb66df9ba5f0
unknown <tivicity2@example.com>
2025-10-04 19:38:54 +0300

    Initial commit

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -4,4 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', TaskListView.as_view(), name="task_list"),
+    path('<int:pk>/', TaskDetailView.as_view(), name="task_detail"),
+    path('task_create', TaskCreateView.as_view(), name="task_create"),
 ]
+
+app_name = "tracker_app"
--- tracker_app/views.py
+++ tracker_app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
 from .models import *
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import TaskForm
 
 
 class TaskListView(ListView):
     model = Task
+    context_object_name = "tasks"
     template_name = "tracker_app/task_list.html"
-    context_object_name = "task_list"
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    context_object_name = "task"
+    template_name = "tracker_app/task_detail.html"
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = "tracker_app/task_form.html"
+    form_class = TaskForm
+    success_url = reverse_lazy("tracker_app:task_list")
